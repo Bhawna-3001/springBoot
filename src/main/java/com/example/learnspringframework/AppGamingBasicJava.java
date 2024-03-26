09d677436b95c467456734127071289523eacb1e
Bhawna-3001 <bagarwal.thapar@gmail.com>
2024-03-26 14:07:21 +0530

    1. loose coupling through interfaces

--- src/main/java/com/example/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/example/learnspringframework/AppGamingBasicJava.java
@@ -7,9 +7,9 @@ import com.example.learnspringframework.game.SuperContra;
 
 public class AppGamingBasicJava {
     public static void main(String[] args){
-//        var game=new SuperContra();
+        var game=new SuperContra();
 //        var marioGame=new MarioGame();
-        var game=new Pacman();
+//        var game=new Pacman();
         var gameRunner=new GameRunner(game);
         gameRunner.run();
 
